91f83e825aaf94972423f78a770c2f209c77b87c
rhl <rhlcoder@gmail.com>
2022-01-01 21:23:01 -0300

    learned '@input'

--- src/app/dbz/dbz.module.ts
+++ src/app/dbz/dbz.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainPageComponent } from './main-page/main-page.component';
 import { FormsModule } from '@angular/forms';
+import { PersonajesComponent } from './personajes/personajes.component';
 
 
 
 @NgModule({
-  declarations: [MainPageComponent],
+  declarations: [MainPageComponent, PersonajesComponent],
   imports: [CommonModule, FormsModule], // importo el forms para hacer magia con los forms. Tambien esta el reactivo, pero es mas avanzado
   exports: [MainPageComponent],
 })
